35b2c6a26a0baeb07018c50371a9a873350f2816
Ahmad Hossain <ahmad.awaab@gmail.com>
2022-03-20 00:27:40 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, TextField, List, ListItem, IconButton, CommentIcon, ListItemText } from '@material-ui/core';
+import { AddCircleOutline } from '@material-ui/icons';
+import React, { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState([])
+    const [text, setText] = useState("");
+
+    const addClicked = () => {
+        //add todo item
+        let newArr = todos.slice();
+        newArr.push(text)
+        setTodos(newArr);
+    }
+    return (
+        <div className="App">
+            <div className='flex-row'>
+                <TextField id="outlined-basic" onChange={e => setText(e.target.value)} label="Outlined" variant="outlined" />
+                <Button variant='contained' onClick={addClicked} startIcon={<AddCircleOutline />}>Add</Button>
+            </div>
+
+            <div className="list">
+                <List>
+                    {todos.map((value) => (
+                        <ListItem
+                            key={value}>
+                            <ListItemText primary={value} />
+                        </ListItem>
+                    ))}
+                </List>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
